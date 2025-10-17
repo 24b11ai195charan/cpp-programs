af6789372f82f5b4896dd5c7ae64330c9466a499
24b11ai195charan <24b11ai195@adityauniversity.in>
2025-10-17 10:38:03 +0530

    Implement basic deque operations in program30.cpp
    Demonstrate deque operations including push and pop.

---
+++ program30.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <deque> 
+using namespace std; 
+int main() { 
+    deque<int> dq; 
+    dq.push_back(10); 
+    dq.push_front(5); 
+    dq.push_back(15); 
+    dq.pop_front(); // removes first element 
+    cout << "Deque Elements: "; 
+    for (int x : dq) cout << x << " "; 
+    cout << endl; 
+    return 0; 
+} 
+ 
+Output: 
+Deque Elements: 10 15
