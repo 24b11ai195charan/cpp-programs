51e26fabc125e13050248e3d467a2f6bb6032f45
24b11ai195charan <24b11ai195@adityauniversity.in>
2025-10-17 10:11:56 +0530

    Add swapNumbers function for call by reference
    Implemented a swap function using call by reference.

--- program2.cpp
+++ program2.cpp
@@ -14,3 +14,22 @@ int main() {
 Output 
 Value of a before function call :10 
 Value of a after function call : 10
+
+    Call by Reference: 
+#include <iostream> 
+using namespace std; 
+void swapNumbers(int &a, int &b) { 
+    int temp = a; 
+    a = b; 
+    b = temp; 
+    cout << "Inside function (after swap): a = " << a << ", b = " << b << endl; 
+} 
+ 
+int main() { 
+    int x = 10, y = 20; 
+    cout << "Before function call: x = " << x << ", y = " << y << endl; 
+    // Call by reference 
+    swapNumbers(x, y); 
+    cout << "After function call: x = " << x << ", y = " << y << endl; 
+    return 0; 
+} 
