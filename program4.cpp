919430bcd45d263bbaf62d8065f953c34bf1ba2e
24b11ai195charan <24b11ai195@adityauniversity.in>
2025-10-17 10:13:40 +0530

    Implement Math and Physics namespaces with add functions

---
+++ program4.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+// Custom namespace 
+namespace Math { 
+    int add(int a, int b) { 
+        return a + b; 
+    } 
+} 
+namespace Physics { 
+    double add(double a, double b) { 
+        return a + b; 
+    } 
+} 
+int main() { 
+    cout << "Sum using Math namespace: " << Math::add(5, 10) << endl; 
+    cout << "Sum using Physics namespace: " << Physics::add(12.5, 7.5) << endl; 
+    return 0; 
+} 
+Output: 
+Sum using Math namespace: 15    Sum using Physics namespace: 20
