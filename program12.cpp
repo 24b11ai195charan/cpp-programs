c0fb4897089401bff241e67630b0963b7567993d
24b11ai195charan <24b11ai195@adityauniversity.in>
2025-10-17 10:20:52 +0530

    Add Demo class with constructor and destructor
    Implement a simple C++ class with a constructor and destructor that outputs messages.

---
+++ program12.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std; 
+class Demo { 
+public: 
+    Demo() {  // Default constructor 
+        cout << "Default Constructor Called!" << endl; 
+    } 
+      ~Demo() { // Destructor 
+        cout << "Destructor Called!" << endl; 
+    } 
+}; 
+int main() { 
+    Demo obj; // Constructor will be called automatically 
+    cout << "Inside main function" << endl; 
+    return 0; // Destructor will be called automatically 
+} 
+ 
+
+ 
+ 
+Output: 
+Default Constructor Called! 
+Inside main function 
+Destructor Called!
